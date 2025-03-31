b08280ada411a2d5c09c1fbd8f087b40e8332d54
domi <domi@domi.kr>
2025-03-31 19:52:09 +0900

    client 앱 워터마크

--- client/src/components/Home/Info.tsx
+++ client/src/components/Home/Info.tsx
@@ -1,5 +1,8 @@
+import style from '@styles/home/style.module.scss';
+
 export default function HomeInfo() {
-    return <section>
-        
+    return <section className={style.info}>
+        <div className={style.copyright}>ⓒ 2025. domi All rights reserved.</div>
+        <div className={style.build}>빌드: v0.0.1 - 2025-03-31 19:50:29<br></br>본 애플리케이션은 학교 내 사용만을 목적으로 하며, 외부에서는 사용할 수 없습니다.</div>
     </section>;
 }
