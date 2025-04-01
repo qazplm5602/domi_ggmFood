0dd9c7164676c8c6b2714dd3638c9beeec7f5762
domi <domi@domi.kr>
2025-04-01 17:12:52 +0900

    client interaction component 끝

--- client/src/components/Content/Interaction/Interaction.tsx
+++ client/src/components/Content/Interaction/Interaction.tsx
@@ -1,9 +1,11 @@
 import style from '@styles/content/style.module.scss';
 
 import arrowIcon from '@assets/icons/left-arrow.svg';
+import homeIcon from '@assets/icons/home.svg';
 
 export default function ContentInteraction() {
     return <section className={style.interaction}>
         <button className={style.back}><img src={arrowIcon} alt='back' />뒤로가기</button>
+        <button className={style.home}><img src={homeIcon} alt="home" /></button>
     </section>;
 }
