9c852bdb3025d0db539e07f02a64f778fb200d45
domi <domi@domi.kr>
2025-04-01 18:58:20 +0900

    client 반 선택 이펙트

--- client/src/components/Content/Class/Class.tsx
+++ client/src/components/Content/Class/Class.tsx
@@ -5,10 +5,10 @@ export default function ContentClass() {
         <h2 className={style.title}>반을 선택하세요.</h2>
 
         <article className={style.classList}>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
+            <button style={{ animationDelay: '0ms' }}>1</button>
+            <button style={{ animationDelay: '100ms' }}>2</button>
+            <button style={{ animationDelay: '200ms' }}>3</button>
+            <button style={{ animationDelay: '300ms' }}>4</button>
         </article>
     </>;
 }
