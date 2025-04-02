37201290369d25ea04d55e1cc00cc9d93cb90cb0
domi <domi@domi.kr>
2025-04-02 15:11:11 +0900

    server 라우팅 구조

--- server/main.ts
+++ server/main.ts
@@ -1,1 +1,10 @@
-console.log("hello world!");
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+
+
+app.listen(process.env.API_PORT, () => console.log(`api listen ${process.env.API_PORT}.`));
