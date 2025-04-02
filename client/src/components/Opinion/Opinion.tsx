254eabacdc4b40f926d15561d9ac7c910a70af41
domi <domi@domi.kr>
2025-04-02 12:32:41 +0900

    기타 의견 추가 표시

--- client/src/components/Opinion/Opinion.tsx
+++ client/src/components/Opinion/Opinion.tsx
@@ -11,8 +11,10 @@ const defaultOpinions = [
     "음식이 너무 짜요",
     "국이 없어요",
     "너무 매워요",
-    "너무 느끼해요",
-    "생선이 싫어요"
+    "음식이 차가워요",
+    "생선이 싫어요 (도미 아님)",
+    "튀김이 눅눅해요",
+    "음식 갯수가 적어요 (제한된 갯수)",
 ];
 const defaultMenuSet = new Set<string>(defaultOpinions);
 
@@ -27,7 +29,12 @@ export default function Opinion({ className, onOpenInput }: Props) {
 
         <section className={style.list}>
             {defaultOpinions.map(v => <OpinionButtonDefault key={`default-${v}`} content={v} />)}
-            {opinions.filter(v => !defaultMenuSet.has(v)).map((v, i) => <button key={`custom-${v}`} className={style.active} onClick={() => handleRemoveOpinion(i)}>{v}</button>)}
+            {opinions.map((v, i) => {
+                if (defaultMenuSet.has(v)) // 이미 있는 메뉴는 제외
+                    return;
+
+                return <button key={`custom-${v}`} className={style.active} onClick={() => handleRemoveOpinion(i)}>{v}</button>;
+            })}
             <button onClick={onOpenInput}>기타</button>
         </section>
     </div>
