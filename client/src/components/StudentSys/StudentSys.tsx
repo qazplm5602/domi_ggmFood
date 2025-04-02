2b92f8a0f11fc7bb759dc09c5814b10b5db9e2db
domi <domi@domi.kr>
2025-04-02 16:35:53 +0900

    client 유저 인덱싱

--- client/src/components/StudentSys/StudentSys.tsx
+++ client/src/components/StudentSys/StudentSys.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
-import { Student } from "@components/Store/student";
+import { Student, StudentsTable, useStudentStore } from "@components/Store/student";
 import StudentSysLoading from "./Loading";
 
 export default function StudentSys() {
     const [ loading, setLoading ] = useState(true);
+    const { setStudents } = useStudentStore();
 
     const loadStudent = async function(aliveRef: { alive: boolean }) {
         setLoading(true);
@@ -12,7 +13,26 @@ export default function StudentSys() {
         const response = await axios.get<Student[]>("/api/students");
         if (!aliveRef.alive) return;
 
-        // ....
+        // 학생 데이터 파싱
+        const result: StudentsTable = {};
+
+        for (const user of response.data) {
+            let grades = result[user.grade];
+            
+            if (!grades) {
+                grades = result[user.grade] = {};
+            }
+            
+            let classes = grades[user.class];
+
+            if (!classes) {
+                classes = grades[user.class] = [];
+            }
+
+            classes.push(user);
+        }
+        setStudents(result);
+
         setLoading(false);
     }
 
