f3a8db1866f83469e4a7b2cdd3e448fed32631d6
domi <domi@domi.kr>
2025-04-02 17:50:41 +0900

    server 평가 저장

--- server/modules/rating.ts
+++ server/modules/rating.ts
@@ -1,4 +1,6 @@
+import { pool } from "./mysql.ts";
 import { ExpressReq, ExpressRes } from "./types.ts";
+import mysql2 from 'mysql2/promise';
 
 interface RatingBodyType {
     star: number,
@@ -7,16 +9,45 @@ interface RatingBodyType {
     mode: 0 | 1 | 2
 }
 
-export function writeFoodRating(req: ExpressReq, res: ExpressRes) {
+export async function writeFoodRating(req: ExpressReq, res: ExpressRes) {
     const { student, mode, star, opinions }: RatingBodyType = req.body;
 
     // 타입 검사
-    if (typeof student !== "number" || !validateMode(mode) || typeof star !== "number" || typeof opinions !== 'object') {
+    if (typeof student !== "number" || !validateMode(mode) || typeof star !== "number" || star < 0 || star > 5 || typeof opinions !== 'object') {
         res.sendStatus(400);
         return;
     }
 
-    
+    const modeString = formatMode(mode);
+
+    // 데베 사용 ㄱㄱ
+    const connection = await pool.getConnection();
+
+    try {
+        await connection.query("INSERT INTO rating(student, mode, star, createAt) VALUES (?, ?, ?, NOW())", [ student, modeString, Math.floor(star) ]);
+        const [ [ { id: ratingId } ] ] = await connection.query("SELECT LAST_INSERT_ID() AS id") as any;
+
+        const opinionQuery = mysql2.format(
+            "INSERT INTO opinions VALUES ?",
+            [[
+                ...opinions.map(v => [ ratingId, v ])
+            ]]
+        );
+
+        // console.log(opinions, opinionQuery);
+        await connection.query(opinionQuery);
+
+        await connection.commit();
+    } catch (e) {
+        await connection.rollback(); // 실패 ~~~~
+        res.sendStatus(500);
+        console.error(e);
+        return;
+    } finally {
+        connection.release();
+    }
+
+    res.send({ ok: true });
 }
 
 // 조 , 중, 식 타입 검사
@@ -28,4 +59,17 @@ function validateMode(mode: any) {
         return false;
 
     return true;
+}
+
+function formatMode(mode: number) {
+    switch (mode) {
+        case 0:
+            return '조';
+        case 1:
+            return '중';
+        case 2:
+            return '석';
+        default:
+            throw new Error(`${mode} faild mode format.`);
+    }
 }
