3c2e5538f66b3548f5ee447d17f53db13163bbaa
domi <domi@domi.kr>
2025-04-02 20:21:24 +0900

    client 0점 이스터에그 부활

--- client/src/components/Star/ZeroStar.tsx
+++ client/src/components/Star/ZeroStar.tsx
@@ -1,8 +1,14 @@
+import { usePopupStore } from '@components/Popup/store';
 import style from '@styles/star/style.module.scss';
 
 export default function StarZero() {
+    const { openPopup, closePopup } = usePopupStore();
+
     const handleClick = function() {
         // alert("예? 0점이요?");
+        openPopup("진짜요????", "예? 0점이요?", [
+            { text: "다시 한번 생각해봐요.", callback: closePopup }
+        ]);
         // alert 말고 다른 방법으로 해야함  (wallpanel 버그 있음)
     }
 
