92e11b496f73db4705780e0c01535edd5b65174d
domi <domi@domi.kr>
2025-04-03 12:21:53 +0900

    client 식사 시간 수정

--- client/src/components/FoodTimeSys/FoodTimeSys.tsx
+++ client/src/components/FoodTimeSys/FoodTimeSys.tsx
@@ -7,10 +7,10 @@ export default function LunchTimeSys() {
     const checkFoodTime = function() {
         const now = new Date();
         const hour = now.getHours();
-        
-        if (hour > 17) {
+
+        if (hour >= 17) {
             setMode(2); // 석식
-        } else if (hour > 12) {
+        } else if (hour >= 12) {
             setMode(1); // 중식
         } else {
             setMode(0); // 조식
