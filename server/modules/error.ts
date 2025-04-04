8eb01c445b1b48816dcc658e56e1cd734ab6a2bb
domi <domi@domi.kr>
2025-04-04 10:43:32 +0900

    server 오류 처리 세분화

--- server/modules/error.ts
+++ server/modules/error.ts
@@ -1,7 +1,31 @@
 import { NextFunction } from "express";
 import { ExpressReq, ExpressRes } from "./types.ts";
 
+export class DomiError extends Error {
+    status: number = -1;
+    code: string = "DOMI0";
+    message: string = "domi-empty";
+
+    constructor(status: number, code: string, message: string) {
+        super(`domiError code: ${code}`);
+        
+        this.status = status;
+        this.code = code;
+        this.message = message;
+    }
+}
+
 export async function errorHandler(err: Error, req: ExpressReq, res: ExpressRes, next: NextFunction) {
     console.error(err);
-    res.sendStatus(500);
+
+    if (!(err instanceof DomiError)) {
+        res.sendStatus(500);
+        return;
+    }
+
+    const domiErr = err as DomiError;
+    res.status(domiErr.status).send({
+        code: domiErr.code,
+        message: domiErr.message
+    });
 }
