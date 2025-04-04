ec5aec95e4c044c9ac1f6cba9e44ad8eea90e30a
domi <domi@domi.kr>
2025-04-04 10:26:19 +0900

    client 폰트 문제 해결

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// import "@styles/font/font.css"; // 엄... 태블릿이랑 폰트가 안맞음
 import "@styles/app/index.scss";
 import App from '@components/App/App';
 
