ba7f57e199f5385f31b90428ec3c90ac3ef15060
domi <domi@domi.kr>
2025-04-05 20:06:48 +0900

    client 평가 중복 대응

--- client/src/components/Content/Head/FinishBtn.tsx
+++ client/src/components/Content/Head/FinishBtn.tsx
@@ -4,12 +4,18 @@ import { motion } from 'framer-motion';
 import { useLoadingStore } from '@components/Store/loading';
 import { useIdentityStore } from '@components/Store/identity';
 import { useRatingStore } from '@components/Store/rating';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import { useFoodTimeStore } from '@components/Store/foodTime';
 import { useScreenStore } from '@components/Store/screen';
+import { usePopupStore } from '@components/Popup/store';
 
 const ANIM_SHOW = { opacity: 1, scale: 1 };
 const ANIM_HIDE = { opacity: 0, scale: 0.9 };
+
+export interface ErrorResponse {
+    code: string,
+    message: string
+}
 
 export default function ContentHeadFinishBtn() {
     const { studentId, step } = useIdentityStore();
@@ -17,6 +23,26 @@ export default function ContentHeadFinishBtn() {
     const { setActive: setLoadingActive } = useLoadingStore();
     const { mode: foodTime } = useFoodTimeStore();
     const { setScreen } = useScreenStore();
+    const { openPopup } = usePopupStore();
+
+    const addErrorAlert = function(err: AxiosError<ErrorResponse>) {
+        if (err.status !== 400) return; // 진짜 먼 오류냐
+
+        const data = err.response?.data;
+        if (!data || data.code !== "RATING0") return;
+
+        // 이미 평가 해버림 (아니 근데 진짜~~~ 왜 동시에 함)
+        const content = <>
+            <p>이미 다른 클라이언트에서 평가 하였습니다.</p>
+            <p>수정하려면 다시 해주세요.</p>
+        </>;
+        const goHome = function() {
+            setLoadingActive(false);
+            setScreen('Home');
+        }
+
+        openPopup("엥", content, [{ text: "확인", callback: goHome }]);
+    }
     
     const handleSubmit = async function() {
         if (step !== 'Star') return; // 어디 화면이여
@@ -33,7 +59,17 @@ export default function ContentHeadFinishBtn() {
         if (id) { // 수정
             await axios.put("./api/rating", data, { params: { id } });
         } else { // 추가 ㅁㄴㅇㄹ
-            await axios.post("./api/rating", data);
+            const success =  await axios.post("./api/rating", data)
+                .then(() => true)
+                .catch(e => {
+                    if (e instanceof AxiosError)
+                        addErrorAlert(e);
+                    
+                    return false;
+                });
+
+            // 오류나면 밑으로 안감
+            if (!success) return;
         }
         
         setLoadingActive(false);
