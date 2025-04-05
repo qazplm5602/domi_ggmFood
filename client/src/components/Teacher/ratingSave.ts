c8adb178d1f849afbce3ea23998090e6877f8101
domi <domi@domi.kr>
2025-04-05 22:21:01 +0900

    client 교사용 평가 api 호출

--- client/src/components/Teacher/ratingSave.ts
+++ client/src/components/Teacher/ratingSave.ts
@@ -1,11 +1,12 @@
 import { useFoodTimeStore } from "@components/Store/foodTime";
 import { useRatingStore } from "@components/Store/rating";
+import axios from "axios";
 
 export default function useSaveTeacherRating() {
     const { star, opinions } = useRatingStore();
     const { mode: foodTime } = useFoodTimeStore();
     
     return async function() {
-        
+        await axios.post("./api/rating/teacher", { star, opinions, mode: foodTime });
     }
 }
