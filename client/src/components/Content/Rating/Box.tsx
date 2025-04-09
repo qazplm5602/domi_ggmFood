5baa254ec121c1154f0f2bf05300f75c769e28c9
domi <domi@domi.kr>
2025-04-09 14:27:11 +0900

    client 기타 도배 방지

--- client/src/components/Content/Rating/Box.tsx
+++ client/src/components/Content/Rating/Box.tsx
@@ -3,25 +3,45 @@ import ContentInputInteraction from './InputInteraction';
 import { motion } from 'framer-motion';
 import { useEffect, useRef, useState } from 'react';
 import { useRatingStore } from '@components/Store/rating';
+import { useLoadingStore } from '@components/Store/loading';
+import { usePopupStore } from '@components/Popup/store';
 
 type Props = {
     onClose?: () => void
 }
 
+const MAX_TEXT_SIZE = 255;
+
 export default function ContentRatingInputBox({ onClose }: Props) {
     const inputRef = useRef<HTMLInputElement>(null);
     const { addOpinion } = useRatingStore();
     const [ value, setValue ] = useState("");
+    const { setActive: setLoadingActive } = useLoadingStore();
+    const { openPopup } = usePopupStore();
 
     const handleClose = function() {
         if (onClose)
             onClose();
     }
     const handleSubmit = function() {
-        // 넣기기
-        if (value.length > 0)
-            addOpinion(value);
+        setLoadingActive(true);
 
+        // 넣기기
+        if (value.length > 0) {
+            if (value.length > MAX_TEXT_SIZE) {
+                const content = <>
+                    <p>할말이 많으신가요?</p>
+                    <p>기타는 짧게 나눠서 입력할 수 있습니다.</p>
+                </>;
+                openPopup("너어어어어어어어무 길어요.", content, [ { text: "확인", callback() {} } ]);
+                setLoadingActive(false); // 자리비움 막기
+                return;
+            }
+
+            addOpinion(value);
+        }
+
+        setLoadingActive(false); // 자리비움 막기
         handleClose();
     }
     const handleChangeValue = function(e: React.ChangeEvent<HTMLInputElement>) {
